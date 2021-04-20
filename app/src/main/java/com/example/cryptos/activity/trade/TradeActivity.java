342fea3562035ffd288ed68a26021c4457669e02
Kadek Widi <kadekwidi@pm.me>
2021-04-20 21:26:08 +0700

    menambahkan beberapa activity dan fragment
    menambahkan fragment baru untuk jual/beli pada TradeActivity, dan
    AboutActivity

---
+++ app/src/main/java/com/example/cryptos/activity/trade/TradeActivity.java
@@ -0,0 +1,16 @@
+package com.example.cryptos.activity.trade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cryptos.R;
+
+public class TradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trade);
+    }
+}
---
+++ app/src/main/java/com/example/cryptos/activity/trade/fragment/BuyFragment.java
@@ -0,0 +1,26 @@
+package com.example.cryptos.activity.trade.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.cryptos.R;
+
+public class BuyFragment extends Fragment {
+    public BuyFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_buy, container, false);
+    }
+}
