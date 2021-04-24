4166baaa9da2ebeef3506495c6ad7300edb59d0f
= <=>
2021-04-25 02:52:12 +0700

    menambahkan listview

--- app/src/main/java/com/example/cryptos/model/Crypto.java
+++ app/src/main/java/com/example/cryptos/model/Crypto.java
@@ -5,37 +5,69 @@ import com.google.firebase.database.DataSnapshot;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Crypto {
-    private Map<String, Double> crypto;
+public  class Crypto {
+    private String name;
+    private String price;
 
-    public Crypto() {
-        crypto = new HashMap<>();
+    public Crypto(String name, String price) {
+        this.name = name;
+        this.price = price;
     }
 
-    public void testPrint() {
-        for(String k : crypto.keySet()) {
-            System.out.println(k + " - " + crypto.get(k));
-        }
+    public String getName() {
+        return name;
     }
 
-    public int setCrypto(DataSnapshot cryptoDataSnapshot) {
-        crypto.clear();
-        if (cryptoDataSnapshot.getKey().equals("crypto")) {
-            cryptoDataSnapshot.getChildren().forEach(child -> {
-                crypto.put(child.getKey(), child.getValue(Double.class));
-            });
-        }
-        return 0;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Map<String, Double> getMap() {
-        return crypto;
+    public String getPrice() {
+        return price;
     }
 
-    public Double getPriceByCurrency(String currency) {
-        for (String c : crypto.keySet()) {
-            if (c.equals(currency)) return crypto.get(currency);
-        }
-        return null;
+    public void setPrice(String price) {
+        this.price = price;
     }
 }
+
+
+
+
+
+
+
+//public class Crypto {
+//    private Map<String, Double> crypto;
+//
+//    public Crypto() {
+//        crypto = new HashMap<>();
+//    }
+//
+//    public void testPrint() {
+//        for(String k : crypto.keySet()) {
+//            System.out.println(k + " - " + crypto.get(k));
+//        }
+//    }
+//
+//    public int setCrypto(DataSnapshot cryptoDataSnapshot) {
+//        crypto.clear();
+//        if (cryptoDataSnapshot.getKey().equals("crypto")) {
+//            cryptoDataSnapshot.getChildren().forEach(child -> {
+//                crypto.put(child.getKey(), child.getValue(Double.class));
+//            });
+//        }
+//        return 0;
+//    }
+//
+//    public Map<String, Double> getMap() {
+//        return crypto;
+//    }
+//
+//    public Double getPriceByCurrency(String currency) {
+//        for (String c : crypto.keySet()) {
+//            if (c.equals(currency)) return crypto.get(currency);
+//        }
+//        return null;
+//    }
+//}
